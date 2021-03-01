b294ca730ea314b13a00a88bf9f955f3e92f45e3
Almir <almirrps@hotmail.com>
2021-03-01 07:44:44 -0300

    Criando o projeto SpringWebflux

---
+++ src/test/java/com/apirest/webfux/WebfuxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirest.webfux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfuxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
